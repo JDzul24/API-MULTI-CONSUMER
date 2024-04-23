08b9bc31b8b06c4fee340b910d63ba27c53771e8
Jonathan Dzul Mendoza <jonidzul@gmail.com>
2024-04-23 00:56:26 -0600

    oki

--- consumer.ts
+++ consumer.ts
@@ -29,7 +29,7 @@ async function getEvent() {
             const idMmalon = parseInt(id)
             console.log(`Message received: ${id}`);
             try {
-                const response = await axios.post('https://api-multi-secundaria.onrender.com', { id_venta: id });
+                const response = await axios.post('https://api-multi-secundaria.onrender.com/registrations', { id_venta: id });
                 console.log(response);
                 
             } catch (error) {
